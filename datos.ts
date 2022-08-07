3ee4ab775926f1630a7591ffaeb5af18c0696526
Alejandro Sebastian Herrero <110503112+Aleherre14@users.noreply.github.com>
2022-08-07 11:19:23 -0300

    Initial commit

---
+++ datos.ts
@@ -0,0 +1,6 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = docuemt.getElementById("rotulo");
+btnEnv.addEventListener("click", () => {
+  alert(dato.value);
+});
